851ccd5b765cc36ff2690e35a03b777277ea7774
hanae-1029 <nichibi1029@icloud.com>
2023-03-14 20:54:52 +0900

    コミット

--- src/main/java/com/techacademy/CountryService.java
+++ src/main/java/com/techacademy/CountryService.java
@@ -1,6 +1,9 @@
 package com.techacademy;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,5 +21,28 @@ public class CountryService {
     public List<Country> getCountryList() {
         // リポジトリのfindAllメソッドを呼び出す
         return repository.findAll();
+    }
+
+    // 一件を検索して返す
+    public Country getCountry(String code) {
+        // findByIdで検索
+        Optional<Country> option = repository.findById(code);
+        // 取得できなかった場合はnullを返す
+        Country country = option.orElse(null);
+        return country;
+    }
+
+    // 更新を行う
+    @Transactional
+    public void updateCountry(String code, String name, int population) {
+        Country country = new Country(code, name, population);
+        repository.save(country);
+
+    }
+
+    // 削除を行う
+    @Transactional
+    public void deleteCountry(String code) {
+        repository.deleteById(code);
     }
 }
